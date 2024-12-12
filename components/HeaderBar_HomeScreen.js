95dae349f9f4152fa838d32692a23a0dac67aad4
De General <aubynsamuel05@gmail.com>
2024-12-12 05:31:43 +0000

    added option to show or hide profile picture in header bar for home screen

--- components/HeaderBar_HomeScreen.js
+++ components/HeaderBar_HomeScreen.js
@@ -11,108 +11,110 @@ import { MaterialIcons } from "@expo/vector-icons";
 import getStyles from "../styles/Component_Styles";
 import { router } from "expo-router";
 
-const TopHeaderBar = memo(({ title, backButtonShown, theme }) => {
-  const { user, logout } = useAuth();
-  const [imageFailed, setImageFailed] = useState(false);
-  const styles = getStyles(theme);
+const TopHeaderBar = memo(
+  ({ title, theme, backButtonShown, profilePicShown = true }) => {
+    const { user, logout } = useAuth();
+    const [imageFailed, setImageFailed] = useState(false);
+    const styles = getStyles(theme);
 
-  const handleLogout = () => {
-    logout();
-    router.replace("login");
-  };
+    const handleLogout = () => {
+      logout();
+      router.replace("login");
+    };
 
-  return (
-    <View style={styles.hhHeaderContainer}>
-      {/* Back Button */}
-      {backButtonShown && (
-        <TouchableOpacity onPress={() => router.navigate("..")}>
-          <MaterialIcons
-            name="arrow-back"
-            style={styles.hhHeaderBarIcon}
-            color={"black"}
-            size={25}
-          />
-        </TouchableOpacity>
-      )}
+    return (
+      <View style={styles.hhHeaderContainer}>
+        {/* Back Button */}
+        {backButtonShown && (
+          <TouchableOpacity onPress={() => router.navigate("..")}>
+            <MaterialIcons
+              name="arrow-back"
+              style={styles.hhHeaderBarIcon}
+              color={"black"}
+              size={25}
+            />
+          </TouchableOpacity>
+        )}
 
-      {/* HeaderTitle */}
-      <Text style={styles.hhHeaderTitle}>{title}</Text>
+        {/* HeaderTitle */}
+        <Text style={styles.hhHeaderTitle}>{title}</Text>
 
-      {/* User Profile Picture */}
-      <View>
-        <Menu>
-          <MenuTrigger>
-            {user?.profileUrl == "" ||
-            user?.profileUrl == null ||
-            imageFailed ? (
-              <Image
-                source={require("../myAssets/Images/default-profile-picture-avatar-photo-600nw-1681253560.webp")}
-                style={{ width: 45, height: 45, borderRadius: 30 }}
-                transition={500}
-              />
-            ) : (
-              <Image
-                source={{ uri: user?.profileUrl }}
-                style={{ width: 45, height: 45, borderRadius: 30 }}
-                transition={500}
-                onError={() => setImageFailed(true)}
-              />
-            )}
-          </MenuTrigger>
-          <MenuOptions
-            style={styles.hhContainer}
-            customStyles={{
-              optionsContainer: {
-                elevation: 5,
-                borderRadius: 10,
-                borderCurve: "circular",
-                marginTop: 40,
-                marginLeft: -30,
-              },
-            }}
-          >
-            {/* Profile */}
-            <MenuOption
-              style={{
-                flexDirection: "row",
-                justifyContent: "space-between",
-                alignItems: "center",
-              }}
-              onSelect={() => {
-                router.navigate("/profile");
+        {/* User Profile Picture */}
+        {profilePicShown && (
+          <Menu>
+            <MenuTrigger>
+              {user?.profileUrl == "" ||
+              user?.profileUrl == null ||
+              imageFailed ? (
+                <Image
+                  source={require("../myAssets/Images/default-profile-picture-avatar-photo-600nw-1681253560.webp")}
+                  style={{ width: 45, height: 45, borderRadius: 30 }}
+                  transition={500}
+                />
+              ) : (
+                <Image
+                  source={{ uri: user?.profileUrl }}
+                  style={{ width: 45, height: 45, borderRadius: 30 }}
+                  transition={500}
+                  onError={() => setImageFailed(true)}
+                />
+              )}
+            </MenuTrigger>
+            <MenuOptions
+              style={styles.hhContainer}
+              customStyles={{
+                optionsContainer: {
+                  elevation: 5,
+                  borderRadius: 10,
+                  borderCurve: "circular",
+                  marginTop: 40,
+                  marginLeft: -30,
+                },
               }}
             >
-              <Text style={styles.hhMenuText}>Profile</Text>
-              <MaterialIcons
-                name="person"
-                color={theme.text.primary}
-                size={25}
-              />
-            </MenuOption>
+              {/* Profile */}
+              <MenuOption
+                style={{
+                  flexDirection: "row",
+                  justifyContent: "space-between",
+                  alignItems: "center",
+                }}
+                onSelect={() => {
+                  router.navigate("/profile");
+                }}
+              >
+                <Text style={styles.hhMenuText}>Profile</Text>
+                <MaterialIcons
+                  name="person"
+                  color={theme.text.primary}
+                  size={25}
+                />
+              </MenuOption>
 
-            {/* Logout */}
-            <MenuOption
-              style={{
-                flexDirection: "row",
-                justifyContent: "space-between",
-                alignItems: "center",
-              }}
-              onSelect={() => {
-                handleLogout();
-              }}
-            >
-              <Text style={styles.hhMenuText}>Sign Out</Text>
-              <MaterialIcons
-                name="logout"
-                color={theme.text.primary}
-                size={25}
-              />
-            </MenuOption>
-          </MenuOptions>
-        </Menu>
+              {/* Logout */}
+              <MenuOption
+                style={{
+                  flexDirection: "row",
+                  justifyContent: "space-between",
+                  alignItems: "center",
+                }}
+                onSelect={() => {
+                  handleLogout();
+                }}
+              >
+                <Text style={styles.hhMenuText}>Sign Out</Text>
+                <MaterialIcons
+                  name="logout"
+                  color={theme.text.primary}
+                  size={25}
+                />
+              </MenuOption>
+            </MenuOptions>
+          </Menu>
+        )}
       </View>
-    </View>
-  );
-});
+    );
+  }
+);
 
 export default TopHeaderBar;
