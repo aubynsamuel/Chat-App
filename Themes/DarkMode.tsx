20890442bc90831465b8f318c4117d4bf99594ca
De General <aubynsamuel05@gmail.com>
2025-01-04 13:35:00 +0000

    added green theme version of dark mode, to be added later

--- Themes/DarkMode.tsx
+++ Themes/DarkMode.tsx
@@ -1,3 +1,4 @@
+// sky blue version of darkTheme
 const darkTheme = {
   primary: "#1E88E5",
   secondary: "#0D47A1",
@@ -34,3 +35,41 @@ const darkTheme = {
 };
 
 export default darkTheme;
+
+// Green Theme Version of darkTheme
+// const darkTheme = {
+//   primary: "green",
+//   secondary: "darkgreen",
+//   background: "#121212",
+//   surface: "#1E1E1E",
+//   readColor: "#FFFFFF",
+//   screenHeaderBarColor: "green",
+//   Statusbar: {
+//     style: "light",
+//     backgroundColor: "#121212",
+//   },
+//   text: {
+//     primary: "#FFFFFF",
+//     secondary: "#AAAAAA",
+//     inverse: "#000000",
+//   },
+//   border: "#404040",
+//   input: {
+//     background: "#FFFFFF",
+//     border: "#fff9",
+//   },
+//   message: {
+//     user: {
+//       background: "green",
+//       text: "#ffffff",
+//       time: "lightgrey",
+//     },
+//     other: {
+//       background: "lightgrey",
+//       text: "#000000",
+//       time: "#000000",
+//     },
+//   },
+// };
+
+// export default darkTheme;
